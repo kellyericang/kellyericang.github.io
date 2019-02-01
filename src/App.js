24cd70860fd7a3384e4f913ea62e959d86e865fc
kellyng <kelly.ng1@outlook.com>
2019-02-01 15:23:12 -0500

    basic files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>wow this is a heading</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
