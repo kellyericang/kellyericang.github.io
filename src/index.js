78eaa44c0445373cba66a06dc709f7baade9fb2d
kellyng <kelly.ng1@outlook.com>
2019-02-05 13:02:53 -0500

    test

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Navbar from './Navbar';
 import * as serviceWorker from './serviceWorker';
 
+ReactDOM.render(<Navbar />, document.getElementById('Navbar'));
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
