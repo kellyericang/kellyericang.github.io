25e1cd0c293acc615077388fa929590d3ea2531e
Kelly Ng <klly.ng@hotmail.com>
2019-02-06 00:05:19 -0500

    updating card poorly

--- src/Card.js
+++ src/Card.js
@@ -1,41 +1,70 @@
 import React, {Component} from 'react';
+import 'tachyons';
 // import './Card.css';
-
-
-		// let resp = await fetch('http://shibe.online/api/shibes?count=1');
-		// let result = await resp.json(); 
-
-		console.log('test');
-		// var shibe = fetch('http://shibe.online/api/shibes?count=1')
-		const shibe = "https://cdn.shibe.online/shibes/ae4aab8ea13dad8bd591afa83285bb6bebb07fbb.jpg";
-
 
 class Card extends Component {
 	constructor() {
 		super();
 		this.state = {
-			pictures: []
+			isLoading: true,
+			shibe: [],
+			error: null
 		}
 	}
 
-	componentDidMount() {
-		fetch('http://shibe.online/api/shibes?count=1')
-		.then(results => {
-			return results.json();
-		}).then(data => {
-			let shibe = data.results.map((pic) => {
-				return <img src={shibe[0]}/>
-			})
-		this.setState({pictures: pictures});
-		console.log('state', this.state.pictures);
+	// fetchShibe() {
+	// 	fetch('http://shibe.online/api/shibes?count=1', {
+	// 		mode: "no-cors"
+	// 	})
+	// 		.then((response) => {
+	// 			console.log('before response.json()');
+	// 			response.json();
+	// 			console.log('after response.json()');
+	// 		})
+	// 		.then(data => {
+	// 			console.log('got the data');
+	// 			console.log(data);
+	// 			this.setState({ 
+	// 				shibe: data,
+	// 				isLoading: false, 
+	// 			});
+	// 		})
+	// 		.catch(error => this.setState({error, isLoading: false}));
+	// }
+
+	async componentDidMount() {
+		const response = await fetch('http://shibe.online/api/shibes?count=1', {
+			mode: "no-cors"
 		})
+		const data = await response.json();
+		this.setState({
+			shibe: data,
+			isLoading: false
+		})
+		// this.fetchShibe();
 	}
 
 	render() {
-
+		const {isLoading, shibe, error} = this.state;
+		console.log(this.state);
 		return (
-			<div className='f1 w-third grow'>
-				{this.state.pictures}
+			<div>
+				{error ? <p>{error.message}</p> : null}
+				{!isLoading ? (
+			        shibe.map(picture => {
+			            const shibe2 = picture;
+			            console.log(shibe2);
+			            return (
+				            <div>
+				              	<p>{shibe2}</p>
+				            </div>
+				        );
+				    })
+				    // If there is a delay in data, let's let the user know it's loading
+				) : (
+				    <h3>Loading...</h3>
+			    )}
+				{this.state.shibe}
 			</div>
 		)
 	}
